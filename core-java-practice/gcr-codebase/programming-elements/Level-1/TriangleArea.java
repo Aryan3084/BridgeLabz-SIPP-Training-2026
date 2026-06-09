7041591b019c97ab95eeb3f992e1a38f9f2dd5ad
Aryan3084 <singhraghav3084@gmail.com>
2026-06-09 10:29:22 +0530

    [Aryan] added core-java_Level-1

---
+++ core-java-practice/gcr-codebase/programming-elements/Level-1/TriangleArea.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+
+        double areaCm = 0.5 * base * height;
+        double areaInch = areaCm / 6.4516;
+
+        System.out.println("Area in square centimeters is " + areaCm + " and square inches is " + areaInch);
+    }
+}
